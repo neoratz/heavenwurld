e7094b490fb0aa0f29f2555531e6b75513bf2e1c
neoratz <185984249+neoratz@users.noreply.github.com>
2026-02-01 15:41:43 -0500

    minor alterations

--- scripts/flavorText.js
+++ scripts/flavorText.js
@@ -9,8 +9,8 @@ let flavorTextOptions = [
     "Dear Neocities Users, All Accounts Will Be Deleted This Following December. Paste This On Your Website To Save Your Website From Being Deleted. Don't Change This Info, Or Else Your Website Will Be Deleted, It Is Because There's About 1,000,000 Fake Accounts. Sorry for This! - Neocities Team",
     "92% of teenagers have turned to Faithstep and Heav N Bass.If you are part of the 8% that still listen to real music, copy and paste this message to another 5 websites. Dont let Angelmusic die.",
     "1)Say your name 10 times 2)say your dog or cat or mouses name 5 times 3)say your bus driver 3 times 4)PASTE THIS TO FOUR OTHER COMMENTS IN 777 MINUTES. WHEN UR DONE PRESS F13 AND UR HEAVENWURLD NAME WILL APPEAR IN BIG LETTERS ON THE SCREEN.",
-    "<3 Enjoy Your Seventh Life <3",
-    "Funnyday some on citrus★mint★candy★drop★kick???"
+    "citrus★mint★candy★drop★kick",
+    "The epicness is over 9000!"
 ];
 
 let flavorTextElement = document.querySelector('.flavorText');
